c6d08d9b0f97faa5d7d0b38f9195aedd5ed73728
Pomax <pomax@nihongoresources.com>
2016-10-23 18:02:21 -0700

    .

--- public/socket.js
+++ public/socket.js
@@ -1,9 +1,16 @@
-var socket = io();
+function bootstrap() {
+	console.log(msg);
+	drawKeyboard();
+	document.addEventListener('keydown', (evt) => { checkSpecialDown(evt); keydown(evt); });
+	document.addEventListener('keyup', (evt) => { checkSpecialDown(evt); keyup(evt); });
+	setupArpeggiator();
+}
 
-socket.on('connected', function(msg){
-  console.log(msg);
-  drawKeyboard();
-  document.addEventListener('keydown', (evt) => { checkSpecialDown(evt); keydown(evt); });
-  document.addEventListener('keyup', (evt) => { checkSpecialDown(evt); keyup(evt); });
-  setupArpeggiator();
-});
+if (io) {
+	var socket = io();
+	socket.on('connected', function(msg){
+		bootstrap();
+	});
+}
+
+else { bootstrap(); }
